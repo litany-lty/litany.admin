92d3e31b4340a2abfcee886d3928d88d6bddd4a8
Litianyu <wb-lty594074@alibaba-inc.com>
2019-12-08 15:34:00 +0800

    git util

---
+++ src/main/java/cn/litany/admin/dto/config/SshConfig.java
@@ -0,0 +1,72 @@
+package cn.litany.admin.dto.config;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.eclipse.jgit.api.TransportConfigCallback;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.transport.Transport;
+import org.eclipse.jgit.util.FS;
+
+import java.io.File;
+
+/**
+ * @author Litany
+ */
+public class SshConfig  {
+
+    public static TransportConfigCallback getSsh(String sshKeyPath){
+        return new TransportConfigCallback() {
+            @Override
+            public void configure(Transport transport) {
+                if (transport instanceof SshTransport) {
+                    SshTransport sshTransport = (SshTransport) transport;
+                    sshTransport.setSshSessionFactory(new Ssh(sshKeyPath));
+                }
+            }
+        };
+    }
+
+    private static class Ssh extends JschConfigSessionFactory {
+
+        private String sshKeyFilePath;
+
+        public Ssh(String sshKeyFilePath) {
+            this.sshKeyFilePath = sshKeyFilePath;
+        }
+
+        @Override
+        protected JSch createDefaultJSch(FS fs ) throws JSchException {
+            JSch defaultJSch = super.createDefaultJSch( fs );
+            defaultJSch.addIdentity( sshKeyFilePath );
+            return defaultJSch;
+        }
+
+        protected JSch createDefaultJSch(final OpenSshConfig.Host hc, FS fs) throws JSchException {
+            JSch jsch = new JSch();
+            jsch.removeAllIdentity();
+            new File(sshKeyFilePath).setReadable(true);
+            jsch.addIdentity(sshKeyFilePath);
+//  todo    jsch.setKnownHosts("C:\\Users\\wb-lty594074\\.ssh\\known_hosts");
+            return jsch;
+        }
+
+        @Override
+        protected void configure(OpenSshConfig.Host hc, Session session) {
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+        }
+
+        public String getSshKeyFilePath() {
+            return sshKeyFilePath;
+        }
+
+        public void setSshKeyFilePath(String sshKeyFilePath) {
+            this.sshKeyFilePath = sshKeyFilePath;
+        }
+    }
+}
--- src/main/java/cn/litany/admin/util/GitUtil.java
+++ src/main/java/cn/litany/admin/util/GitUtil.java
@@ -1,10 +1,72 @@
 package cn.litany.admin.util;
 
+import cn.litany.admin.dto.config.SshConfig;
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.aspectj.util.FileUtil;
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.transport.Transport;
+
+import java.io.File;
+import java.util.Date;
+
 /**
  * @author Litany
  */
 public class GitUtil {
 
+    public static Boolean cloneRepository(String username, String keyPath) {
 
+        CloneCommand cloneCommand = Git.cloneRepository();
+        File directory = new File(ConfigUtil.getUserOfficialPath(username));
+        if (directory.exists()) {
+            FileUtil.deleteContents(directory);
+        }
 
+        cloneCommand.setDirectory(directory);
+        cloneCommand.setURI("git@github.com:" + username + "/" + username + ".github.io.git");
+        cloneCommand.setTransportConfigCallback(SshConfig.getSsh(keyPath));
+        try {
+            cloneCommand.call();
+            return true;
+        } catch (GitAPIException e) {
+            return false;
+        }
+
+    }
+
+    public static Boolean pull(String username, String keyPath) {
+        try {
+            Git git = Git.open(new File(ConfigUtil.getUserOfficialPath(username)));
+            PullCommand pull = git.pull();
+            pull.setTransportConfigCallback(SshConfig.getSsh(keyPath));
+            pull.call();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public static Boolean push(String username, String keyPath) {
+        try {
+            File dir = new File(ConfigUtil.getUserOfficialPath(username));
+            Git git = Git.open(dir);
+            git.add().addFilepattern(".").call();
+            git.commit().setMessage(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:ss:mm") + "Auto update").call();
+            PushCommand push = git.push();
+            push.setTransportConfigCallback(SshConfig.getSsh(keyPath));
+            push.call();
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
 }
+
+
+
